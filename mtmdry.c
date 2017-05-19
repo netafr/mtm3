ae9744b48fc982205c2c6b2441fac6d4a6da34f3
Neta <netafr@campus.technion.ac.il>
2017-05-19 11:29:28 +0000

    dry q2

--- mtmdry.c
+++ mtmdry.c
@@ -1,4 +1,35 @@
 #include <stdio.h>
+#include <assert.h>
+
+/*to add to git:
+git add.
+git commit -m "comment"
+git push -u origin [branch]
+*/
+
+/*Function gets array, its size, a compare function. and the object to find
+    the compare function returns positive number if the first value is larger
+    than the second and vice versa. */
+int binaryFind(void** arr, int arr_size, int(*compareFunc)(void *, void *), 
+                                                            void* to_find) {
+    int high = arr_size - 1;
+    int low = 0;
+    int mid = 0;
+    while (low <= high) {
+        mid = (high + low)/2;
+        if(compareFunc(to_find, arr[mid]) == 0) {
+            return mid;
+        }
+        else if(compareFunc(to_find, arr[mid]) > 0) {
+            low = mid + 1;
+        } else {
+            assert(compareFunc(to_find, arr[mid]) < 0);
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
 
 int main() {
     
